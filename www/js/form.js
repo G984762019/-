2ab0310b9b4009a2503d5e01e9a384469c64ecff
G984762019 <198476@st.takushoku-u.ac.jp>
2022-11-12 01:40:02 +0900

    ファイルストアまで

--- www/js/form.js
+++ www/js/form.js
@@ -1,18 +1,11 @@
 // JavaScript file
 var year, mon, day, hour, min;
-
+var imagenumber = 1;
 var applicationKey = "87013227784c0efa8a57de9169a06e041fb8ebb674682411dd4713c82fd301a2";
 var clientKey = "bdddce7180688334da89518e0b556941061215f38e8f7dcfc1616453b6bf61ec";
 const appId = 'pD3voLarW8vLV4q';
 // SDKの初期化
 var ncmb = new NCMB(applicationKey, clientKey);
-
-
-
-//画像
-
-
-
 
 
 //位置情報を取得
@@ -38,32 +31,52 @@ function geoget2(position) {
     geo.textContent = geo_text;
 }
 
-//ローカルストレージ
+//データストアへの保存
 function SousinnButton() {
 
-
-
     var time = document.getElementById("time").value;
-    var image = document.getElementById("image").value;
     var select = document.getElementById("select").value;
     var text = document.getElementById("text").value;
-    alert("実行済み");
 
 
 
-    // mobile backend に保存
-    // 保存先クラスの生成
-    var MessageClass = ncmb.DataStore("MessageClass");
-    // クラスインスタンスの生成
-    var messageClass = new MessageClass();
-    // データを設定して保存する
-    messageClass.set("time", time)
-        .set("image", image)
-        .set("ido", ido)
-        .set("keido", keido)
-        .set("text", text)
-        .set("select", select)
-        .save();
+    //画像データ
 
-    alert("実行済み2");
+    // 画像番号の取得
+    var ImageClass = ncmb.DataStore("ImageClass");
+    var imageClass = new ImageClass();
+    var item = ImageClass
+        .fetchById("Jh4OLchzUs15KdTE")
+        .then(function (item) {
+            //ファイルストアに画像を保存
+            var fileData = document.getElementById("image").files[0];
+
+            ncmb.File.upload(item.imagenumber, fileData)
+                .then(function (res) {
+                    // アップロード後処理
+                    alert("データは" + item.imagenumber);
+                    // mobile backend に保存
+                    // 保存先クラスの生成
+                    var MessageClass = ncmb.DataStore("MessageClass");
+                    // クラスインスタンスの生成
+                    var messageClass = new MessageClass();
+                    // データを設定して保存する
+                    messageClass.set("time", time)
+                        .set("image", item.imagenumber)
+                        .set("ido", ido)
+                        .set("keido", keido)
+                        .set("text", text)
+                        .set("select", select)
+                        .save();
+
+                    alert("実行済み");
+                    //画像番号の更新
+                    imagenumber = item.imagenumber + 1;
+                    item.set("imagenumber", imagenumber)
+                        .update();
+                })
+
+
+        })
+
 }
